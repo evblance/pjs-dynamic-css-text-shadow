89e3b0dd43029f451bb6ce30406226513b26938d
evblance <evblance@users.noreply.github.com>
2019-05-14 11:34:24 +1000

    Listens to mousemove events on the document and adds logic for retriving the window centre and mouse positions

--- shadows.js
+++ shadows.js
@@ -17,5 +17,38 @@ function setCSSVariable(name, value) {
   document.documentElement.style.setProperty(`--${name}`, value);
 }
 
+/**
+ * Returns a Position object containing the (x, y) coordinates of the mouse relative to the client window.
+ * @param {any} mouseEV The mouse event to use in deriving the mouse position.
+ */
+function getMousePositionByEvent(mouseEV) {
+  const mouseX = mouseEV.clientX;
+  const mouseY = mouseEV.clientY;
+  return new Position(mouseX, mouseY);
+}
+
+/**
+ * Returns a Position object containing the (x, y) coordinates of the client window centre.
+ */
+function getWindowCentre() {
+  const height = window.innerHeight;
+  const width = window.innerWidth;
+  return new Position(width / 2, height / 2);
+}
+
+/**
+ * Handles mousemove to drive dynamic text shadow behaviour.
+ * @param {any} event The client mousemove event.
+ */
+function handleMouseMove(event) {
+  const mousePos = getMousePositionByEvent(event);
+  console.log(`Mouse Pos: (${mousePos.x}, ${mousePos.y})`);
+  const windowCentrePos = getWindowCentre();
+  console.log(`Client origin: (${windowCentrePos.x}, ${windowCentrePos.y})`);
+}
+
 // References to the DOM for manipulating element text shadow(s)
 const heading = document.getElementById('text');
+
+// Listen to mousemove events so we can make our text shadow dynamic
+document.addEventListener('mousemove', handleMouseMove);
